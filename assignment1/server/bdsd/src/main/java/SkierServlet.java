f3fef42c67e0b0d2028854ced02db6a9a3d417c8
simulated-annealing <tang.hao.husky@gmail.com>
2020-10-06 15:44:39 -0700

    add server project

---
+++ assignment1/server/bdsd/src/main/java/SkierServlet.java
@@ -0,0 +1,44 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SkierServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setStatus(HttpServletResponse.SC_CREATED);
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("application/json");
+        String urlPath = req.getPathInfo();
+
+        // check we have a URL!
+        if (urlPath == null || urlPath.isEmpty()) {
+            res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            res.getWriter().write("missing paramterers");
+            return;
+        }
+
+        String[] urlParts = urlPath.split("/");
+        // and now validate url path and return the response status code
+        // (and maybe also some value if input is valid)
+
+        if (!isUrlValid(urlParts)) {
+            res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } else {
+            res.setStatus(HttpServletResponse.SC_OK);
+            // do any sophisticated processing with urlParts which contains all the url params
+            // TODO: process url params in `urlParts`
+            //res.getWriter().write("It works!");
+            res.getWriter().write("{}");
+            res.getWriter().flush();
+        }
+    }
+
+    private boolean isUrlValid(String[] urlPath) {
+        // TODO: validate the request url path according to the API spec
+        // urlPath  = "/1/seasons/2019/day/1/skier/123"
+        // urlParts = [, 1, seasons, 2019, day, 1, skier, 123]
+        return true;
+    }
+}
