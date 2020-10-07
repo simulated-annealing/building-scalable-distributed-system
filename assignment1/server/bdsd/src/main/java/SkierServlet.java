c5671dc250d0f5b1055d181e9118f6e549b6790f
tanghao <tang.hao2@husky.neu.edu>
2020-10-07 13:22:42 -0700

    add content and url validation

--- assignment1/server/bdsd/src/main/java/SkierServlet.java
+++ assignment1/server/bdsd/src/main/java/SkierServlet.java
@@ -1,11 +1,35 @@
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.regex.*;
 
 public class SkierServlet extends javax.servlet.http.HttpServlet {
+
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        response.setStatus(HttpServletResponse.SC_CREATED);
+        String url = request.getPathInfo();
+        if (!isUrlValid(url)) {
+            response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+            return;
+        }
+
+        // json validation
+        try {
+            BufferedReader streamReader = new BufferedReader( new InputStreamReader(request.getInputStream(), "UTF-8"));
+            StringBuilder responseStrBuilder = new StringBuilder();
+            String inputStr;
+            while ((inputStr = streamReader.readLine()) != null)
+                responseStrBuilder.append(inputStr);
+            JSONObject.parseObject(responseStrBuilder.toString());
+            response.setStatus(HttpServletResponse.SC_CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+        }
     }
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
@@ -15,30 +39,30 @@ public class SkierServlet extends javax.servlet.http.HttpServlet {
         // check we have a URL!
         if (urlPath == null || urlPath.isEmpty()) {
             res.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            res.getWriter().write("missing paramterers");
+            res.getWriter().write("missing parameters");
             return;
         }
 
-        String[] urlParts = urlPath.split("/");
-        // and now validate url path and return the response status code
-        // (and maybe also some value if input is valid)
-
-        if (!isUrlValid(urlParts)) {
+        if (!isUrlValid(urlPath)) {
             res.setStatus(HttpServletResponse.SC_NOT_FOUND);
         } else {
             res.setStatus(HttpServletResponse.SC_OK);
-            // do any sophisticated processing with urlParts which contains all the url params
-            // TODO: process url params in `urlParts`
-            //res.getWriter().write("It works!");
-            res.getWriter().write("{}");
+            res.getWriter().write("{}");// return a dummy json.
             res.getWriter().flush();
         }
     }
 
-    private boolean isUrlValid(String[] urlPath) {
-        // TODO: validate the request url path according to the API spec
-        // urlPath  = "/1/seasons/2019/day/1/skier/123"
-        // urlParts = [, 1, seasons, 2019, day, 1, skier, 123]
+    private boolean isUrlValid(String url) {
         return true;
+        /*
+        // GET /skiers/{skierID}/vertical
+        Pattern pat1 = Pattern.compile("\\/[0-9]+\\/vertical");
+        // GET /skiers/{resortID}/days/{dayID}/skiers/{skierID}
+        Pattern pat2 = Pattern.compile("\\/[A-Za-z0-9]+\\/days\\/[0-9]+\\/skiers\\/[0-9]+");
+        // POST /skiers/liftrides
+        Pattern pat3 = Pattern.compile("\\/liftrides");
+
+        return pat1.matcher(url).matches() || pat2.matcher(url).matches() || pat3.matcher(url).matches();
+         */
     }
 }
